1d785661081cc699580c65179793f30fdb5501ae
J.S. Easley <easleyjs@gmail.com>
2019-02-01 20:40:00 -0800

    Fixed Lab 7

--- lab7.py
+++ lab7.py
@@ -10,36 +10,38 @@ Allow the user to choose how many characters the password will be.
 Advanced Version 2
 Allow the user to choose how many letters, numbers, and punctuation characters they want in their password. Mix everything up using list(), random.shuffle(), and ''.join().
 """
-import random
+from random import randint
 
-punctuation_list = [33,34,39,44,46,58,59,63,96]
-letters_list = []
+def getLetterList():
+    return [chr(x) for x in (list(range(65,91)) + list(range(97,123)))]
+
+def getNumberList():
+    return [chr(x) for x in range(48,57)]
+
+def getPunctuationList():
+    static_punctuation_list = [33,34,39,44,46,58,59,63,96]
+    return [chr(x) for x in static_punctuation_list]
+
+def getRandomCharacters(charType,numChars):
+    if charType == 'letter':
+        source_list = getLetterList()
+    elif charType == 'number':
+        source_list = getNumberList()
+    elif charType == 'punctuation':
+        source_list = getPunctuationList()
+    return "".join([source_list[randint(0,len(source_list)-1)] for x in range(0,numChars)])
 
 passwordString = ""
-
 passwordLength = int(input("How many characters long would you like your password to be?: "))
 numLetters = int(input("How many letters would you like in your password?: "))
 numNumbers = int(input("How many numbers would you like in your password?: "))
 numPunctuation = int(input("How many punctuation characters would you like in your password?: "))
 
-for x in range(passwordLength):
+if numNumbers > 0:
+    passwordString += getRandomCharacters('number',numNumbers)
+if numLetters > 0:
+    passwordString += getRandomCharacters('letter',numLetters)
+if numPunctuation > 0:
+    passwordString += getRandomCharacters('punctuation',numPunctuation)
 
-    if numNumbers > 0:
-        for i in range(0,numNumbers):
-            passwordString += chr(random.randint(48,57))
-
-    if numLetters > 0:
-        for i in range(65,90):
-            letters_list.append(chr(i))
-
-        for x in range(97,122):
-            letters_list.append(chr(x))
-
-        for y in range(0,numLetters-1):
-            passwordString += letters_list[random.randint(0,len(letters_list)-1)]
-
-    if numPunctuation > 0:
-        for i in range(0,numLetters-1):
-            passwordString += chr(punctuation_list[random.randint(0,len(punctuation_list)-1)])
-
-print(passwordString)
+print("Your password is: " + passwordString)
