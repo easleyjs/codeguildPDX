2dac0b458524a4e391051889a5b1af397702dac4
J.S. Easley <easleyjs@gmail.com>
2019-02-05 21:15:53 -0800

    Lab 13 initial

---
+++ lab13.py
@@ -0,0 +1,8 @@
+#lab13.py
+"""
+Write a program that prompts the user for a string, and encodes it with ROT13.
+For each character, find the corresponding character, add it to an output string.
+Notice that there are 26 letters in the English language, so encryption is the same as decryption.
+"""
+
+inputString = input("Enter a string for encryption: ")
