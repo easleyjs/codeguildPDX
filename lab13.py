c04bfbec6d1c46cb7286ceb17102cee5ba9b5eb7
J.S. Easley <easleyjs@gmail.com>
2019-02-06 21:23:19 -0800

    Lab 14

--- lab13.py
+++ lab13.py
@@ -4,7 +4,7 @@ Write a program that prompts the user for a string, and encodes it with ROT13.
 For each character, find the corresponding character, add it to an output string.
 Notice that there are 26 letters in the English language, so encryption is the same as decryption.
 """
-##Using the ASCII range (97-123) and chr/ord.
+##Using the ASCII range 97-123 and chr/ord.
 
 #Encryption
 def encryptString(inputString):
