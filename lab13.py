f5d520ce8b7d043b2b3b0f1c4d2909ef7ac8afd3
J.S. Easley <easleyjs@gmail.com>
2019-02-06 20:48:51 -0800

    Initial Lab 13 Push

--- lab13.py
+++ lab13.py
@@ -4,5 +4,30 @@ Write a program that prompts the user for a string, and encodes it with ROT13.
 For each character, find the corresponding character, add it to an output string.
 Notice that there are 26 letters in the English language, so encryption is the same as decryption.
 """
+##Using the ASCII range (97-123) and chr/ord.
+
+#Encryption
+def encryptString(inputString):
+    encryptedString = ""
+    for c in inputString:
+        encryptedString += chr(97+(ord(c)-97+13)-26)
+    return encryptedString
+
+#Decryption
+def decryptString(inputString):
+    decryptedString = ""
+    for c in inputString:
+        decryptedString += chr(((ord(c)+13)-97)+97)
+    return decryptedString
 
 inputString = input("Enter a string for encryption: ")
+encryptedString = ""
+rotAmt = 13
+
+encryptedString = encryptString(inputString)
+
+print(f"Encrypted String: {encryptedString}")
+
+decryptedString = decryptString(encryptedString)
+
+print(f"Decrypted String: {decryptedString}")
