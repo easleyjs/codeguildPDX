c4b07d7b601fe6b258ac51b769933e5dfc78da1e
J.S. Easley <easleyjs@gmail.com>
2019-02-15 01:47:44 -0800

    finished Lab 18

--- lab18.py
+++ lab18.py
@@ -9,7 +9,38 @@ peaks_and_valleys - uses the above two functions to compile a single list of the
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 def peaks(data):
+    """
+    >>> print(peaks(data))
+    [6, 14]
+    """
     return [i for i in range(1, len(data)-1) if (data[i] > data[i-1]) & (data[i] > data[i+1])]
 
-print(data)
-print(peaks(data))
+
+def valleys(data):
+    """
+    >>> print(valleys(data))
+    [9, 17]
+    """
+    return [i for i in range(1, len(data)-1) if (data[i] < data[i-1]) & (data[i] < data[i+1])]
+
+
+def peaks_and_valleys(data):
+    """
+    >>> print(peaks_and_valleys(data))
+    [6, 9, 14, 17]
+    """
+    return sorted(peaks(data) + valleys(data))
+
+
+def printGraph(data):
+    """
+    """
+    graphString = ""
+    for i in range(max(data), 0, -1):
+        for j in data:
+            if j >= i:
+                graphString += "X "
+            else:
+                graphString += "  "
+        graphString += "\n"
+    return graphString
