399d5f7ced5a54f3fc91cbb6c0bda205992ff50a
J.S. Easley <easleyjs@gmail.com>
2019-02-20 17:38:51 -0800

    update

--- lab21.py
+++ lab21.py
@@ -11,6 +11,7 @@ from string import punctuation
 s = 'I $am a !string with punc&^%*tuation!'
 translator = str.maketrans('', '', punctuation)
 string_without_punct = s.translate(translator) # I am a string with punctuation
+
 Your dictionary will have words as keys and counts as values. If a word isn't in your dictionary yet, add it with a count of 1. If it is, increment its count.
 Print the most frequent top 10 out with their counts. You can do that with the code below.
 # word_dict is a dictionary where the key is the word and the value is the count
@@ -22,7 +23,11 @@ You'll find that the most common words aren't particularly interesting (lots of 
 
 STOPWORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 """
+from string import punctuation
 with open('dunwichhorror', 'r') as f:
     contents = f.read()
+translator = str.maketrans('', '', punctuation)
+book = contents.lower().translate(translator)
 
-print(contents[0])
+
+print(book)
