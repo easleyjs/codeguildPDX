3527cc1f45fa13886ad95712afb3515694f19643
J.S. Easley <easleyjs@gmail.com>
2019-02-25 20:01:39 -0800

    Lab 25 started

---
+++ lab25.py
@@ -0,0 +1,9 @@
+"""
+Let's represent an ATM with a class containing a balance property. A newly created account will default to a balance of 0. Implement the initializer, as well as the following methods:
+
+check_balance() returns the account balance
+deposit(amount) deposits the given amount in the account
+check_withdrawal(amount) returns true if the withdrawn amount won't put the account in the negative
+withdraw(amount) withdraws the amount from the account and returns it
+
+"""
