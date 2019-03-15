3b498c0757357b0b7824067be286002e132c9154
J.S. Easley <easleyjs@gmail.com>
2019-03-14 18:41:00 -0700

    HTML folder created, lab01 done.

--- Lab26.py
+++ Lab26.py
@@ -28,9 +28,60 @@ is_game_over() Returns true if the game board is full or a player has won.
 class Game():
     def __init__(self, numPlayers):
         self.numPlayers = numPlayers
-        self.board = [[], [], []]
+        self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
     def __repr__(self):
-        boardDisplay = "+" + '-'*30 + "+\n"
-        for i in self.board:
-            boardDisplay += "|" + "|".join(self.board[i])
+        boardDisplay = "+" + '-'*5 + "+\n"
+        for i in range(len(self.board)):
+            for x in range(len(self.board[i])):
+                boardDisplay += "|" + str(self.board[i][x])
+            boardDisplay += "|\n"
+        boardDisplay += "+" + '-'*5 + "+\n"
+        return boardDisplay
+
+    def move(self, y, x, token):
+        """Place a player's token character string at a given coordinate
+        (top-left is 0, 0), x is horizontal position, y is vertical position.
+        """
+        self.x = x
+        self.y = y
+        self.token = token
+        self.board[self.x][self.y] = self.token
+
+    def calc_winner(self, token):
+        """
+        What token character string has won or None if no one has.
+        """
+        self.token = token
+        hasWon = False
+        lineString = ""
+        for i in range(len(self.board)):
+            horizontalString = self.board[i][0] + self.board[i][1] + self.board[i][2]
+            verticalString = self.board[0][i] + self.board[1][i] + self.board[2][i]
+        if (horizonString == self.token*3) or :
+
+class Player():
+    def __init__(self, playerName, token):
+        self.playerName = playerName
+        self.token = token
+
+
+    def __repr__(self):
+        return self.playerName + ": " + self.token
+
+
+def main():
+    players = []
+    numPlayers = int(input("How many players are there?\n> "))
+    for i in range(numPlayers):
+        print(f"\nPlayer {i+1}\n")
+        playerName = input("What is the player's name?\n> ")
+        token = str(input("What token will this player use?\n> "))
+        players.append(Player(playerName, token))
+    game = Game(1)
+    game.move(0, 1, "O")
+    print(game)
+    print(players)
+
+if __name__ == '__main__':
+    main()
